9f92f5dd24b639c321cb64bad24d5de0c592530e
yoshiryo <ryota.yoshizaki.2r@stu.hosei.ac.jp>
2021-11-02 18:40:07 +0900

    add comments

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from time import sleep
 import pytchat #pychatをインポート
 import time
 chat = pytchat.create(video_id="") #対象動画のurlを指定
-while chat.is_alive():
-    for c in chat.get().items:
+while chat.is_alive(): #動画が続いていれば
+    for c in chat.get().items: #チャットを取得
         print(c.datetime, c.message)
-    time.sleep(5)
+    time.sleep(5) #sleepを入れないと空のデータが作成される可能性
